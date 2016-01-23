7854a7f866a88a888b018cc2e52cfc3d98f6e5ba
Brad Busenius <bradb@neomailbox.ch>
2016-01-23 17:43:49 -0600

    Wrapped the run script in a function.

--- run.py
+++ run.py
@@ -20,14 +20,21 @@
 
 from savings_rate import SRConfig, SavingsRate, Plot
 
-# Instantiate a savings rate config object
-config = SRConfig('ini', 'config/config.ini')
+def run():
+    """
+    Run the application in, "ini" mode.
+    """
+    # Instantiate a savings rate config object
+    config = SRConfig('ini', 'config/config.ini')
 
-# Instantiate a savings rate object for a user
-savings_rate = SavingsRate(config)
-monthly_rates = savings_rate.get_monthly_savings_rates()
+    # Instantiate a savings rate object for a user
+    savings_rate = SavingsRate(config)
+    monthly_rates = savings_rate.get_monthly_savings_rates()
 
-# Plot the user's savings rate
-user_plot = Plot(savings_rate)
-user_plot.plot_savings_rates(monthly_rates)
+    # Plot the user's savings rate
+    user_plot = Plot(savings_rate)
+    user_plot.plot_savings_rates(monthly_rates)
+
+
+run()
 
