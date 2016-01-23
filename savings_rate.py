0423af837fd0e0a35fceb338cc39dc9b297ff878
Brad Busenius <bradb@neomailbox.ch>
2016-01-23 16:18:51 -0600

    Created an application launcher.

--- savings_rate.py
+++ savings_rate.py
@@ -830,18 +830,3 @@ class Plot:
             show(p)
         else:
             return components(p)
-    
-
-# Instantiate a savings rate config object
-#config = SRConfig('ini', 'config/config.ini')
-
-# TEST config fr postgres integration
-#config =  SRConfig('postgres', 'config/config.ini')
-   
-# Instantiate a savings rate object for a user
-#savings_rate = SavingsRate(config)
-#monthly_rates = savings_rate.get_monthly_savings_rates()
-
-# Plot the user's savings rate
-#user_plot = Plot(savings_rate)
-#user_plot.plot_savings_rates(monthly_rates)
